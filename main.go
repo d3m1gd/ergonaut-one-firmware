82cca792c529e6eb48e2089add2ace8309d5035e
Max D <d3m1gd@gmail.com>
2025-04-04 17:37:07 +0700

    better render

--- main.go
+++ main.go
@@ -9,12 +9,66 @@ import (
 	"slices"
 	"strings"
 	"text/template"
+
+	"github.com/BooleanCat/go-functional/v2/it"
 )
 
-type LevelName string
+type Behavior interface {
+	Behavior() string
+}
 
-var LevelNames = []LevelName{
-	"BASE",
+type Layer map[RC]Behavior
+type LayerSeq = iter.Seq2[RC, Behavior]
+type LayerName string
+
+func (l Layer) Render() []string {
+	widths := slices.Repeat([]int{0}, 12)
+	cells := make([][]string, 4)
+	for i := range cells {
+		cells[i] = slices.Repeat([]string{""}, 12)
+	}
+	for n := range 42 {
+		n := n + 1
+		rc := RCFrom(n)
+		b := l[rc]
+		rendered := b.Behavior()
+		row := rc.Row - 1
+		col := rc.Col - 1
+		if rc.Side == Right {
+			col += 6
+		} else {
+			if rc.Row == 4 {
+				col += 3
+			}
+		}
+		cells[row][col] = rendered
+		widths[col] = max(widths[col], len(rendered))
+	}
+
+	for _, row := range cells {
+		for j := range row {
+			row[j] = fmt.Sprintf("%-*s", widths[j], row[j])
+		}
+	}
+
+	return slices.Collect(it.Map(slices.Values(cells), func(ss []string) string {
+		s := strings.Join(ss, " ")
+		s = strings.TrimRight(s, " ")
+		return s
+	}))
+}
+
+func (ln LayerName) Less(other LayerName) int {
+	return cmp.Compare(ln, other)
+}
+
+var LayerNames = []LayerName{
+	"BASEXX",
+}
+
+type RenderedLayer struct {
+	Name LayerName
+	Rows []string
 }
 
 const Miss = "MISSING"
@@ -35,35 +89,54 @@ func (s Side) Short() string {
 	panic("unhandled side: " + s)
 }
 
-type Layer struct {
-	Name string
-	Keys []Key
-}
-
 type ToMacro struct {
-	Level int
-	RC    RC
-	Key   Key
+	Level    int
+	RC       RC
+	Behavior string
 }
 
 type Params struct {
 	ToBaseAnd []ToMacro
+	Layers    []RenderedLayer
 }
 
-type Key struct {
-	Action      string
-	Other       string
-	Constructor func(string, string) string
+type KpKp struct {
+	Tap  string
+	Hold string
 }
 
-func (b Key) Behavior() string {
-	return b.Constructor(b.Action, b.Other)
+func (x KpKp) Behavior() string {
+	return fmt.Sprintf("&kpkp %s %s", x.Hold, x.Tap)
+}
+
+type Trans struct{}
+
+func (_ Trans) Behavior() string {
+	return "&trans"
 }
 
 type RC struct {
 	Side Side
 	Row  int
 	Col  int
+}
+
+func RCFrom(n int) RC {
+	row := (n-1)/12 + 1
+	col := (n-1)%12 + 1
+	side := Left
+	if col > 6 {
+		side = Right
+		col -= 6
+	}
+	if row == 4 {
+		if col > 3 {
+			side = Right
+			col -= 3
+		}
+	}
+
+	return RC{side, row, col}
 }
 
 func r(a, b int) RC {
@@ -102,75 +175,61 @@ func (rc RC) Less(other RC) int {
 	return cmp.Compare(rc.Serial(), other.Serial())
 }
 
-func KpKp(a, b string) string {
-	return fmt.Sprintf("&kpkp %s %s", b, a)
-}
-
-func Trans(_, _ string) string {
-	return "&trans"
-}
-
-var all = []Key{}
-
-func InitAllTrans() []Key {
-	keys := make([]Key, 42)
-	for i := range keys {
-		keys[i] = Key{"", "", Trans}
+func InitTrans() Layer {
+	layer := Layer{}
+	for i := range 42 {
+		layer[RCFrom(i+1)] = Trans{}
 	}
 
-	return keys
+	return layer
 }
 
-var layers = map[string]map[RC]Key{}
+var layers = map[LayerName]Layer{}
 
 func init() {
-	layers["BASE"] = map[RC]Key{}
-	// layers["BASE"][l(1, 1)] = Key{"a", "b", KpKp}
-	// layers["BASE"][l(1, 2)] = Key{"a", "b", KpKp}
-	// layers["BASE"][l(1, 3)] = Key{"a", "b", KpKp}
-	// layers["BASE"][l(1, 4)] = Key{"a", "b", KpKp}
-	// layers["BASE"][l(1, 5)] = Key{"a", "b", KpKp}
-	// layers["BASE"][l(1, 6)] = Key{"a", "b", KpKp}
-	// layers["BASE"][r(1, 1)] = Key{"a", "b", KpKp}
-	// layers["BASE"][r(1, 2)] = Key{"a", "b", KpKp}
-	// layers["BASE"][r(1, 3)] = Key{"a", "b", KpKp}
-	// layers["BASE"][r(1, 4)] = Key{"a", "b", KpKp}
-	// layers["BASE"][r(1, 5)] = Key{"a", "b", KpKp}
-	// layers["BASE"][r(1, 6)] = Key{"a", "b", KpKp}
-	// layers["BASE"][l(2, 1)] = Key{"a", "b", KpKp}
-	// layers["BASE"][l(2, 2)] = Key{"a", "b", KpKp}
-	// layers["BASE"][l(2, 3)] = Key{"a", "b", KpKp}
-	// layers["BASE"][l(2, 4)] = Key{"a", "b", KpKp}
-	// layers["BASE"][l(2, 5)] = Key{"a", "b", KpKp}
-	// layers["BASE"][l(2, 6)] = Key{"a", "b", KpKp}
-	// layers["BASE"][r(2, 1)] = Key{"a", "b", KpKp}
-	// layers["BASE"][r(2, 2)] = Key{"a", "b", KpKp}
-	// layers["BASE"][r(2, 3)] = Key{"a", "b", KpKp}
-	// layers["BASE"][r(2, 4)] = Key{"a", "b", KpKp}
-	// layers["BASE"][r(2, 5)] = Key{"a", "b", KpKp}
-	// layers["BASE"][r(2, 6)] = Key{"a", "b", KpKp}
-	// layers["BASE"][l(3, 1)] = Key{"a", "b", KpKp}
-	// layers["BASE"][l(3, 2)] = Key{"a", "b", KpKp}
-	// layers["BASE"][l(3, 3)] = Key{"a", "b", KpKp}
-	// layers["BASE"][l(3, 4)] = Key{"a", "b", KpKp}
-	// layers["BASE"][l(3, 5)] = Key{"a", "b", KpKp}
-	// layers["BASE"][l(3, 6)] = Key{"a", "b", KpKp}
-	// layers["BASE"][r(3, 1)] = Key{"a", "b", KpKp}
-	layers["BASE"][r(3, 2)] = Key{"M", "RG(M)", KpKp}
-	layers["BASE"][r(3, 3)] = Key{"COMMA", "RG(COMMA)", KpKp}
-	layers["BASE"][r(3, 4)] = Key{"DOT", "RG(DOT)", KpKp}
-	// layers["BASE"][r(3, 5)] = Key{"a", "b", KpKp}
-	// layers["BASE"][r(3, 6)] = Key{"a", "b", KpKp}
-	// layers["BASE"][l(4, 1)] = Key{"a", "b", KpKp}
-	// layers["BASE"][l(4, 2)] = Key{"a", "b", KpKp}
-	// layers["BASE"][l(4, 3)] = Key{"a", "b", KpKp}
-	// layers["BASE"][r(4, 1)] = Key{"a", "b", KpKp}
-	// layers["BASE"][r(4, 2)] = Key{"a", "b", KpKp}
-	// layers["BASE"][r(4, 3)] = Key{"a", "b", KpKp}
-
-	// BASE.Keys[r(3, 2).Serial()]={r(3, 2), "M", "RG(M)", KpKp}
-
-	// all = append(all, r32, r33, r34)
+	layers["BASEXX"] = InitTrans()
+	// layers["BASEXX"][l(1, 1)] = Key{"a", "b", KpKp}
+	// layers["BASEXX"][l(1, 2)] = Key{"a", "b", KpKp}
+	// layers["BASEXX"][l(1, 3)] = Key{"a", "b", KpKp}
+	// layers["BASEXX"][l(1, 4)] = Key{"a", "b", KpKp}
+	// layers["BASEXX"][l(1, 5)] = Key{"a", "b", KpKp}
+	// layers["BASEXX"][l(1, 6)] = Key{"a", "b", KpKp}
+	// layers["BASEXX"][r(1, 1)] = Key{"a", "b", KpKp}
+	// layers["BASEXX"][r(1, 2)] = Key{"a", "b", KpKp}
+	// layers["BASEXX"][r(1, 3)] = Key{"a", "b", KpKp}
+	// layers["BASEXX"][r(1, 4)] = Key{"a", "b", KpKp}
+	// layers["BASEXX"][r(1, 5)] = Key{"a", "b", KpKp}
+	// layers["BASEXX"][r(1, 6)] = Key{"a", "b", KpKp}
+	// layers["BASEXX"][l(2, 1)] = Key{"a", "b", KpKp}
+	// layers["BASEXX"][l(2, 2)] = Key{"a", "b", KpKp}
+	// layers["BASEXX"][l(2, 3)] = Key{"a", "b", KpKp}
+	// layers["BASEXX"][l(2, 4)] = Key{"a", "b", KpKp}
+	// layers["BASEXX"][l(2, 5)] = Key{"a", "b", KpKp}
+	// layers["BASEXX"][l(2, 6)] = Key{"a", "b", KpKp}
+	// layers["BASEXX"][r(2, 1)] = Key{"a", "b", KpKp}
+	// layers["BASEXX"][r(2, 2)] = Key{"a", "b", KpKp}
+	// layers["BASEXX"][r(2, 3)] = Key{"a", "b", KpKp}
+	// layers["BASEXX"][r(2, 4)] = Key{"a", "b", KpKp}
+	// layers["BASEXX"][r(2, 5)] = Key{"a", "b", KpKp}
+	// layers["BASEXX"][r(2, 6)] = Key{"a", "b", KpKp}
+	// layers["BASEXX"][l(3, 1)] = Key{"a", "b", KpKp}
+	// layers["BASEXX"][l(3, 2)] = Key{"a", "b", KpKp}
+	// layers["BASEXX"][l(3, 3)] = Key{"a", "b", KpKp}
+	// layers["BASEXX"][l(3, 4)] = Key{"a", "b", KpKp}
+	// layers["BASEXX"][l(3, 5)] = Key{"a", "b", KpKp}
+	// layers["BASEXX"][l(3, 6)] = Key{"a", "b", KpKp}
+	// layers["BASEXX"][r(3, 1)] = Key{"a", "b", KpKp}
+	layers["BASEXX"][r(3, 2)] = KpKp{"M", "RG(M)"}
+	layers["BASEXX"][r(3, 3)] = KpKp{"COMMA", "RG(COMMA)"}
+	layers["BASEXX"][r(3, 4)] = KpKp{"DOT", "RG(DOT)"}
+	// layers["BASEXX"][r(3, 5)] = Key{"a", "b", KpKp}
+	// layers["BASEXX"][r(3, 6)] = Key{"a", "b", KpKp}
+	// layers["BASEXX"][l(4, 1)] = Key{"a", "b", KpKp}
+	// layers["BASEXX"][l(4, 2)] = Key{"a", "b", KpKp}
+	// layers["BASEXX"][l(4, 3)] = Key{"a", "b", KpKp}
+	// layers["BASEXX"][r(4, 1)] = Key{"a", "b", KpKp}
+	// layers["BASEXX"][r(4, 2)] = Key{"a", "b", KpKp}
+	// layers["BASEXX"][r(4, 3)] = Key{"a", "b", KpKp}
 }
 
 func renderKeymap(path string, params Params) {
@@ -180,11 +239,22 @@ func renderKeymap(path string, params Params) {
 	check(t.Execute(outFile, params))
 }
 
-func MapToBaseAnd(seq iter.Seq2[RC, Key]) iter.Seq[ToMacro] {
+func LayerToBaseAndSeq(seq LayerSeq) iter.Seq[ToMacro] {
 	return func(yield func(ToMacro) bool) {
 		for rc, key := range seq {
-			tm := ToMacro{0, rc, key}
+			tm := ToMacro{0, rc, key.Behavior()}
 			if !yield(tm) {
+				return
+			}
+		}
+	}
+}
+
+func RenderLayerSeq(seq iter.Seq2[LayerName, Layer]) iter.Seq[RenderedLayer] {
+	return func(yield func(RenderedLayer) bool) {
+		for name, layer := range seq {
+			rl := RenderedLayer{name, layer.Render()}
+			if !yield(rl) {
 				return
 			}
 		}
@@ -212,7 +282,8 @@ func SortedMap[K Lesser[K], V any](m map[K]V) iter.Seq2[K, V] {
 
 func main() {
 	params := Params{
-		ToBaseAnd: slices.Collect(MapToBaseAnd(SortedMap(layers["BASE"]))),
+		Layers:    slices.Collect(RenderLayerSeq(SortedMap(layers))),
+		ToBaseAnd: slices.Collect(LayerToBaseAndSeq(SortedMap(layers["BASEXX"]))),
 	}
 
 	renderKeymap("config/ergonaut_one.keymap", params)
