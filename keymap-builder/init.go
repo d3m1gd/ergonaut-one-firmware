a9735d5b194606041918fd9ed05078ec66bdd111
Max D <d3m1gd@gmail.com>
2025-05-02 11:53:08 +0700

    no idle for parens

--- keymap-builder/init.go
+++ keymap-builder/init.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"keyboard/chain"
+	"keyboard/combo"
 	. "keyboard/instance"
 	"keyboard/key"
 	. "keyboard/key/keys"
 	"keyboard/layer"
 	"keyboard/ref"
+	"keyboard/rowcol"
 )
 
 var (
@@ -143,5 +145,87 @@ func init() {
 
 	chain.Add(CHAINS, InitWith(To(BASE)), map[string]ref.T{
 		"sdf": Kp(X),
+	})
+
+	combo.Add(combo.T{
+		Name:   "System",
+		Refs:   []ref.T{ref1("sll", SYS)}, // todo
+		Keys:   []rowcol.T{L15, L16},
+		IdleMs: 500,
+	})
+
+	combo.Add(combo.T{
+		Name: "LeftEnter",
+		Refs: []ref.T{Kp(RETURN)},
+		Keys: []rowcol.T{L42, L43},
+	})
+
+	combo.Add(combo.T{
+		Name: "LeftSpace",
+		Refs: []ref.T{Kp(SPACE)},
+		Keys: []rowcol.T{L41, L42},
+	})
+
+	combo.Add(combo.T{
+		Name: "RightCaps",
+		Refs: []ref.T{CapsWord},
+		Keys: []rowcol.T{R23, R24},
+	})
+
+	combo.Add(combo.T{
+		Name:     "MiddleMouse",
+		Refs:     []ref.T{MKp(MCLK)},
+		Keys:     []rowcol.T{L34, L35},
+		IdleMs:   200,
+		TimoutMs: 100,
+	})
+
+	combo.Add(combo.T{
+		Name:     "Curlies",
+		Refs:     []ref.T{Curlies()},
+		Keys:     []rowcol.T{L25, R22},
+		TimoutMs: 50,
+	})
+
+	combo.Add(combo.T{
+		Name:     "Parens",
+		Refs:     []ref.T{Parens()},
+		Keys:     []rowcol.T{L24, R23},
+		TimoutMs: 50,
+	})
+
+	combo.Add(combo.T{
+		Name:     "Brackets",
+		Refs:     []ref.T{Brackets()},
+		Keys:     []rowcol.T{L23, R24},
+		TimoutMs: 50,
+	})
+
+	combo.Add(combo.T{
+		Name:     "DoubleQuotes",
+		Refs:     []ref.T{DoubleQuotes()},
+		Keys:     []rowcol.T{L15, R12},
+		TimoutMs: 50,
+	})
+
+	combo.Add(combo.T{
+		Name:     "SingleQuotes",
+		Refs:     []ref.T{SingleQuotes()},
+		Keys:     []rowcol.T{L14, R13},
+		TimoutMs: 50,
+	})
+
+	combo.Add(combo.T{
+		Name:     "BackQuotes",
+		Refs:     []ref.T{BackQuotes()},
+		Keys:     []rowcol.T{L13, R14},
+		TimoutMs: 50,
+	})
+
+	combo.Add(combo.T{
+		Name:     "CodeQuotes",
+		Refs:     []ref.T{ref0("mdCode")}, // todo
+		Keys:     []rowcol.T{L16, R11},
+		TimoutMs: 50,
 	})
 }
